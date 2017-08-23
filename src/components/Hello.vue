9ae9c8f5e70e2ba0021f30b7bd2a3b7945b5020a
threetown <threetowns@163.com>
2017-08-23 15:49:00 +0800

    [Vux] Demo: vux components.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="mainWarp">
+    <group title="prop:content">
+      <x-switch title="显示" v-model="show2"></x-switch>
+    </group>
+    <div>
+      <alert v-model="show2" title="温馨提示" content="这里是内容了"></alert>
+    </div>
+
   </div>
 </template>
 
 <script>
+
+import { Alert, Group, XSwitch } from 'vux'
+
+
+
 export default {
-  name: 'hello',
+
+  components: {
+    Alert,
+    Group,
+    XSwitch
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      show2: false
     }
+  },
+  methods: {
+
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="less">
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 
-a {
-  color: #42b983;
-}
 </style>
