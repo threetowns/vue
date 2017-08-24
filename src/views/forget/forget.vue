303f5e321fd1708dc258fb47287ff79ff3019379
threetown <threetowns@163.com>
2017-08-24 19:17:06 +0800

    [Tpl] Add: forget password template

---
+++ src/views/forget/forget.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login-layout">
+
+
+    <div class="login-form" v-if="!isNext">
+      <div class="login-items">
+        <i class="icon-card"></i>
+        <input type="text" class="login-text" placeholder="请输入手机号或者邮箱地址">
+      </div>
+      <button class="login-button" type="button">下一步</button>
+    </div>
+
+    <div class="login-form" v-else>
+      <div class="login-items">
+        <i class="icon-check"></i>
+        <input type="text" class="login-text" placeholder="输入验证码">
+        <button class="sent-sms" type="button">获取验证码</button>
+      </div>
+      <div class="login-items">
+        <i class="icon-lock"></i>
+        <input type="password" class="login-text" placeholder="请输入密码">
+      </div>
+      <button class="login-button" type="button">完成</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isNext: false
+    }
+  }
+}
+</script>
+<style lang="less">
+   @import '~assets/styles/login.less';
+   .login-tips{
+    text-align: center;
+   }
+</style>
