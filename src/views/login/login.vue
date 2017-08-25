097da93d7719a17d547bcc8efb21f68007f291e2
threetown <threetowns@163.com>
2017-08-25 10:14:25 +0800

    [vux] Add: use md5 feature.

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,15 +4,15 @@
     <div class="login-form">
       <div class="login-items">
         <i class="icon-card"></i>
-        <input type="text" class="login-text" placeholder="请输入手机号或者邮箱地址">
+        <input type="text" class="login-text" v-model="username" :placeholder="placeholder.username">
       </div>
       <div class="login-items">
         <i class="icon-lock"></i>
-        <input type="password" class="login-text" placeholder="请输入密码">
+        <input type="password" class="login-text" v-model="password" :placeholder="placeholder.password">
       </div>
     </div>
 
-    <button class="login-button" type="button">登录</button>
+    <button class="login-button" type="button" @click="checkLogin">登录</button>
     <div class="login-tips">
       <router-link to="/join" class="to-join">快速注册</router-link>
       <router-link to="/forget" class="to-forget">忘记密码？</router-link>
@@ -22,7 +22,25 @@
 </template>
 
 <script>
+  import { md5 } from 'vux';
 
+  export default {
+    data(){
+      return {
+        username: '',
+        password: '',
+        placeholder: {
+          username: '请输入手机号或者邮箱地址',
+          password: '请输入密码'
+        }
+      }
+    },
+    methods:{
+      checkLogin(){
+        console.log(md5(this.username))
+      }
+    }
+  }
 </script>
 <style lang="less">
    @import '~assets/styles/login.less';
