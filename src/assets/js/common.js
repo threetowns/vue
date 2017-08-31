46fb093ee4e3e858b671fa22e9a70fb93df16253
threetown <threetowns@163.com>
2017-08-31 17:37:07 +0800

    [TEST] Mod: test API domain.

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -29,7 +29,7 @@ var imitationQuery = {
       },
       // `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
       // 它可以通过设置一个 `baseURL` 便于为 axios 实例的方法传递相对 URL
-      baseURL: (opts.baseURL != undefined) ? opts.baseURL : 'http://172.16.50.57:8080/',
+      baseURL: (opts.baseURL != undefined) ? opts.baseURL : 'http://172.16.10.216:8080/',
       timeout: opts.time || 10 * 1000,
       responseType: opts.dataType || 'json'
     };
